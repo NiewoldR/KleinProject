35974c40a9d35e1755a174e09d6056ee446f72ea
Raymond Loman <raymond@carpago.nl>
2019-06-18 19:12:08 +0200

    Add integration test and failsafe

---
+++ src/test/java/com/niewold/r/player/ApplicationIT.java
@@ -0,0 +1,19 @@
+package com.niewold.r.player;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApplicationIT {
+
+    @Test
+    public void contextLoads() {
+        Assert.assertTrue(true); // to have some test and to have spring boot running ...
+    }
+
+
+}
